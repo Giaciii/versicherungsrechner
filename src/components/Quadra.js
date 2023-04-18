1872e2968c4b38f9338bf48efbd3b4831900e4fb
Giaciii <giacun.masueger@stud.kbw.ch>
2023-04-18 11:10:59 +0200

    Collabs Menü konnte hinzugefügt werden, <useCollabs> muss in den Klammern sein

--- src/components/Quadra.js
+++ src/components/Quadra.js
@@ -1,10 +1,26 @@
 import '../App.css';
-import {Collapse} from 'react-collapse';
+import React from 'react';
+import {useCollapse} from 'react-collapsed';
+
+function Collapsible() {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+return (
+    <div className="collapsible">
+        <div className="header" {...getToggleProps()}>
+            {isExpanded ? 'Collapse' : 'Expand'}
+        </div>
+        <div {...getCollapseProps()}>
+            <div className="content">
+                Now you can see the hidden content. <br/><br/>
+                Click again to hide...
+            </div>
+        </div>
+    </div>
+    );
+}
 
 export default function Quadra() {
     return (
-        <Collapse isOpened={true || false}>
-            <div>Random content</div>
-        </Collapse>
+        <Collapsible/>
     );
 }
